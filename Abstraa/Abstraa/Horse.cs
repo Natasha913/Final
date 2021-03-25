1bc99ea085bfeac3fecea34d59ad9c46c266dacf
natasha <natashakainat91@gmail.com>
2021-03-25 21:30:13 +0600

    Abstraction

---
+++ Abstraa/Abstraa/Horse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraa
+{
+    class Horse:Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Horse eat with mouth");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Horse sleep by standing");
+        }
+
+        public override void Walk()
+        {
+            Console.WriteLine("Horse walk by lags");
+        }
+    }
+}
