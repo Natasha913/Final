e8dede0fdd2cd3a0d257a4fc21d83234e6968e7e
natasha <natashakainat91@gmail.com>
2021-04-19 22:39:55 +0600

    delegate

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate void Calc(int a, int b);
+
+        public static void Add(int a, int b)
+        {
+             Console.WriteLine("Sum is :" + (a+b));
+        }
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine("Sub is :" + (a - b));
+        }
+        static void Main(string[] args)
+        {
+
+            int a = 24, b = 22;
+            //Add(a, b); or calculator(a, b); same
+
+            Calc calculator = Add;
+            calculator(a, b);
+
+             calculator  += Sub;
+            calculator(4,2);
+        }
+    }
+}
